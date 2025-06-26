c621c7acb4fd3aea259fb3c43f5eab95be8aecf1
Will Osbourne <will@osbourne.org>
2025-06-26 08:31:16 -0400

    remove unused src files

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// Export the web component
-export { default as OhShtButton } from './oh-sht-button.js';
-
-// Auto-register the web component when this file is imported
-import './oh-sht-button.js';
